83527a5c9772f2dc393d2527a77e7e2094529a6c
Johannes Huwald <=>
2020-06-21 13:08:20 +0200

    added button to mark highlights

--- static/js/ui.js
+++ static/js/ui.js
@@ -18,3 +18,53 @@ document.addEventListener('mouseup', e => {
     ui.style.left = `${r.x+window.pageXOffset}px`;
     ui.style.top = `${r.y+window.pageYOffset+LINE_HEIGHT}px`;
 });
+
+function createHighlight(type) {
+    let sel = document.getSelection();
+
+    for (let i=0; i<sel.rangeCount; ++i) {
+        let r = sel.getRangeAt(i);
+        let start = r.startContainer;
+        let end = r.endContainer;
+
+        if (start.nodeType === 1)
+            start = start.childNodes[0];
+
+        console.log(start.nodeType, end.nodeType);
+
+        if (start == end) {
+            let s = document.createElement('span');
+            s.classList.add('highlight', 'noselect');
+            s.innerText = start.data.substr(r.startOffset, r.endOffset-r.startOffset);
+
+            end = start.data.substr(r.endOffset, start.data.length);
+            start.data = start.data.substr(0, r.startOffset);
+            start.parentNode.insertBefore(s, start.nextSibling);
+            s.insertAdjacentText('afterend', end);
+
+            s.addEventListener('click', e => console.log("Clicked", e));
+
+
+        } else {
+            let s = document.createElement('span');
+            s.classList.add('highlight', 'noselect');
+            s.innerText = start.data.substr(r.startOffset, start.data.length);
+
+            start.data = start.data.substr(0, r.startOffset);
+            start.parentNode.insertBefore(s, start.nextSibling);
+
+            if (end.nodeType == 3) {
+                s = document.createElement('span');
+                s.classList.add('highlight', 'noselect');
+                s.innerText = end.data.substr(0, r.endOffset);
+
+                end.data = end.data.substr(r.endOffset, end.data.length);
+                end.parentNode.insertBefore(s, end);
+            }
+        }
+
+        ui.classList.add('hidden');
+    }
+
+    sel.empty();
+}
